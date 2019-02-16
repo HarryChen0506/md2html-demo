cc6d98f5b29c7cda6ef2dd99c113ddd1cc3433a1
chenwenhua <wenhua.chen@versa-ai.com>
2019-02-16 12:08:25 +0800

    init project

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MdEditor from 'rc-md2html'
+
+import './index.less'
+// const mock_text = "Hello.\n\n * This is markdown.\n * It is fun\n * Love it or leave it."
+const mock_text = `
+  ### [Emojies](https://github.com/markdown-it/markdown-it-emoji)
+
+  > Classic markup: :wink: :crush: :cry: :tear: :laughing: :yum:
+  >
+  > Shortcuts (emoticons): :-) :-( 8-) ;)
+
+  see [how to change output](https://github.com/markdown-it/markdown-it-emoji#change-output) with twemoji.
+`
+
+class Hello extends React.Component {
+  constructor (props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+    this.state = {
+      count: 1
+    }
+  } 
+  
+  handleClick (e) {
+    // console.log('click me', e)
+    const count = this.state.count
+    this.setState({
+      count: count + 1
+    })
+  }
+
+  renderCount () {    
+    return <span name="span">{this.state.count}</span>       
+  }
+
+  onEditorChange ({html, text, show}) {    
+    console.log('onEditorChange', html, text, show)
+  }
+
+  render () {
+    return (
+      <div className="wrap">
+        {this.renderCount()}
+        <button onClick={this.handleClick}>click me</button>  
+        <MdEditor value={mock_text} defaultShow="md" mdStyle={{color: '#fff'}} onChange={this.onEditorChange} />         
+      </div>      
+    )
+  }
+}
+
+const Demo = (
+  <div className={'title'} key={'title'}>
+    <Hello name="harry"/>    
+  </div>
+)
+
+ReactDOM.render(
+  Demo,
+  document.getElementById('root')
+)
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const config = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    port: '8081',
+    host: '0.0.0.0',  //支持ip来访问页面，否则只能通过localhost:8080来访问
+    historyApiFallback: true,  //所有404页面能跳转到index.html
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader', 
+          'css-loader', 
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './public/index.html'),
+      filename: 'index.html',
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+};
+
+module.exports = config;
