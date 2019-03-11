7d062484081c256d4eae934586c9bab6b073e063
chenwenhua <wenhua.chen@versa-ai.com>
2019-03-11 14:36:45 +0800

    v0.3.9

--- src/demo.js
+++ src/demo.js
@@ -14,6 +14,29 @@ class Demo extends React.Component {
     // console.log('handleEditorChange', text)
   }
 
+  handleImageUpload = (file, callback) => {
+    const reader = new FileReader()
+    reader.onload = () => {      
+      const convertBase64UrlToBlob = (urlData) => {  
+        let arr = urlData.split(','), mime = arr[0].match(/:(.*?);/)[1]
+        let bstr = atob(arr[1])
+        let n = bstr.length
+        let u8arr = new Uint8Array(n)
+        while (n--) {
+          u8arr[n] = bstr.charCodeAt(n)
+        }
+        return new Blob([u8arr], {type:mime})
+      }
+      const blob = convertBase64UrlToBlob(reader.result)
+      setTimeout(() => {
+        // setTimeout 模拟oss异步上传图片
+        // 当oss异步上传获取图片地址后，执行calback回调（参数为imageUrl字符串），即可将图片地址写入markdown
+        callback('https://avatars0.githubusercontent.com/u/21263805?s=40&v=4')
+      }, 1000)
+    }
+    reader.readAsDataURL(file)
+  }
+
   handleGetMdValue = () => {
     if (this.mdEditor) {
       alert(this.mdEditor.getMdValue())      
@@ -45,7 +68,8 @@ class Demo extends React.Component {
             },
             // synchScroll: false
           }}
-          onChange={this.handleEditorChange} 
+          onChange={this.handleEditorChange}
+          onImageUpload={this.handleImageUpload}
         />  
       </div> 
     )
